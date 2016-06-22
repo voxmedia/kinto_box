7aea633955cd4066ec98327b4b8a8489ce8cf54c
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-22 16:45:17 -0700

    error classes

---
+++ lib/api_errors.rb
@@ -0,0 +1,29 @@
+module KintoBox
+  # Base exception class for all API Client exceptions
+  class Error < StandardError
+    attr_reader :response, :data
+    def initialize(response)
+      @response = response
+      @data ||= begin
+        JSON.parse(response.body)
+      rescue
+        {}
+      end
+
+      super("#{response.code} #{response.request.uri} #{response.body}")
+    end
+  end
+
+  # Raised when there was something wrong with the request
+  class BadRequest < Error; end
+
+  # Raised when the authentication information is incorrect or incomplete
+  class NotAllowed < Error; end
+
+  # Raised when the requested thing is not found
+  class NotFound < Error; end
+
+  # Raised when there is some sort of error on the server
+  class ServerError < Error; end
+
+end
