36d324d3b481bed0e38a215cdc82fd2aa5932b91
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-22 16:45:27 -0700

    add tests

--- test/kinto_box_test.rb
+++ test/kinto_box_test.rb
@@ -5,7 +5,26 @@ class KintoBoxTest < Minitest::Test
     refute_nil ::KintoBox::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_get_server_info
+    kinto_client = KintoBox::KintoClient.new('https://kintobox.herokuapp.com', nil)
+    resp = kinto_client.serverInfo
+    assert_equal resp['project_name'], 'kinto'
+    assert_equal resp['url'], 'https://kintobox.herokuapp.com/v1/'
+  end
+
+  def test_get_server_info_w_auth
+    kinto_client = KintoBox::KintoClient.new('https://kintobox.herokuapp.com', {:username => 'test', :password => 'my-secret'})
+    resp = kinto_client.serverInfo
+    assert_equal resp['project_name'], 'kinto'
+    assert_equal resp['url'], 'https://kintobox.herokuapp.com/v1/'
+  end
+
+  def test_non_existent_server
+    kinto_client = KintoBox::KintoClient.new('http://kavyasukumar.com')
+
+    assert_raises KintoBox::NotFound do
+      resp = kinto_client.serverInfo
+    end
   end
 end
+
