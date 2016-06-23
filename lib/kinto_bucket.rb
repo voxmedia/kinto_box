096dd2a20917e455b8c8c932c72a0aadb776fcac
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-23 15:25:09 -0700

    bucket info function

--- lib/kinto_bucket.rb
+++ lib/kinto_bucket.rb
@@ -1,4 +1,15 @@
 module KintoBox
   class KintoBucket
+    attr_accessor :id
+
+    def initialize (client, bucket_id)
+      @kinto_client = client
+      raise ArgumentError if bucket_id.nil?
+      @id = bucket_id
+    end
+
+    def info
+      @kinto_client.get("/buckets/#{@id}")
+    end
   end
 end
