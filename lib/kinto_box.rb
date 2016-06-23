18e5105ef2e3f78c3ec2356b4780047dc2ea2855
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-23 15:25:28 -0700

    refactor auth

--- lib/kinto_box.rb
+++ lib/kinto_box.rb
@@ -1,6 +1,8 @@
 require "kinto_box/version"
 require 'httparty'
 require 'response_handler'
+require 'kinto_bucket'
+require 'base64'
 
 module KintoBox
   class KintoClient
@@ -8,21 +10,28 @@ module KintoBox
     headers 'Accept' => 'application/json', 'Content-Type' => 'application/json'
     format :json
 
-    attr_accessor :default_bucket
-
     def initialize(server, options = nil)
       @server = server
       self.class.base_uri URI.join(@server, '/v1/').to_s
 
-      @auth = {username: options[:username],
-               password: options[:password] } unless options.nil?
+      unless options.nil? || options[:username].nil? || options[:password].nil?
+        @auth = Base64.encode64("#{options[:username]}:#{options[:password]}")
+      end
 
-      @auth = ENV['KINTO_API_TOKEN'] unless ENV['KINTO_API_TOKEN'].nil?
+      @auth = ENV['KINTO_API_TOKEN'] if @auth.nil?
+      self.class.headers('Authorization' => "Basic #{@auth}")
     end
 
-    def serverInfo
+    def bucket (bucket_id)
+      @bucket = KintoBucket.new(self, bucket_id)
+      @bucket
+    end
+
+    def server_info
       get '/'
     end
+
+    def
 
     def get(path)
       ResponseHandler.handle self.class.get(path)
