70c5c87b463612941c2d80ef9909e64d22c79fec
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-23 16:27:50 -0700

    add delete for bucket

--- lib/kinto_bucket.rb
+++ lib/kinto_bucket.rb
@@ -6,10 +6,15 @@ module KintoBox
       @kinto_client = client
       raise ArgumentError if bucket_id.nil?
       @id = bucket_id
+      @url_path = "/buckets/#{@id}"
     end
 
     def info
-      @kinto_client.get("/buckets/#{@id}")
+      @kinto_client.get(@url_path)
+    end
+
+    def delete
+      @kinto_client.delete(@url_path)
     end
   end
 end
