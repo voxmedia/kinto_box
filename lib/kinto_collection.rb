eef4e675fc139c526d5d53acb1503bb6c2dadf22
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-23 17:14:34 -0700

    add basic functions

--- lib/kinto_collection.rb
+++ lib/kinto_collection.rb
@@ -1,2 +1,18 @@
-class KintoCollection
+module KintoBox
+  class KintoCollection
+    def initialize (bucket, collection_id)
+      raise ArgumentError if bucket.nil? || collection_id.nil?
+      @kinto_client = bucket.kinto_client
+      @id = collection_id
+      @url_path = "/buckets/#{bucket.id}/collections/#{@id}"
+    end
+
+    def info
+      @kinto_client.get(@url_path)
+    end
+
+    def delete
+      @kinto_client.delete(@url_path)
+    end
+  end
 end
